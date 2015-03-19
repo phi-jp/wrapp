21acee0a01e5e369dba9a1c4d3aaa0de7ed01259
phi <phi1618jp@gmail.com>
2015-03-19 21:58:51 +0900

    add social message plugin

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -6,12 +6,20 @@ module.exports = [
         "clobbers": [
             "window.AdMob"
         ]
+    },
+    {
+        "file": "plugins/uk.co.ilee.socialmessage/www/socialmessage.js",
+        "id": "uk.co.ilee.socialmessage.SocialMessage",
+        "clobbers": [
+            "socialmessage"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "com.google.cordova.admob": "2.7.4",
+    "uk.co.ilee.socialmessage": "0.2.6",
     "com.rjfun.cordova.extension": "1.0.6",
     "com.google.playservices": "19.0.0"
 }
