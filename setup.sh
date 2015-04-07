7c11882a776450a65ba34a9225692eb6eb589fcc
phi <phi1618jp@gmail.com>
2015-04-07 11:35:17 +0900

    add setup file

---
+++ setup.sh
@@ -0,0 +1,4 @@
+chmod +x platforms/ios/cordova/build
+chmod +x platforms/ios/build/FlickArrow.build/Debug-iphonesimulator/FlickArrow.build/Script-304B58A110DAC018002A0835.sh
+chmod +x platforms/ios/cordova/lib/copy-www-build-step.sh
+chmod +x platforms/ios/cordova/run
